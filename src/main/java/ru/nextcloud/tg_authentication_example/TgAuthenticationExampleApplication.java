65aa520e25a313ca78d67b5f6861a57480082175
Alex <ford4601@yandex.ru>
2025-05-27 17:20:32 +0300

    init

---
+++ src/main/java/ru/nextcloud/tg_authentication_example/TgAuthenticationExampleApplication.java
@@ -0,0 +1,13 @@
+package ru.nextcloud.tg_authentication_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgAuthenticationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgAuthenticationExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nextcloud/tg_authentication_example/TgAuthenticationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nextcloud.tg_authentication_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgAuthenticationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
